4100a478d2ead446b932ea44bff9282ee75bd2f6
rodriguezh21 <rodriguezh21@gmail.com>
2020-06-05 15:04:57 -0500

    Added models folder

---
+++ Develop/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan")
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+const db = require("./models");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false });
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use(logger("dev"));
+
+
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+})
